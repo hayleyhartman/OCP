6b51681f29f524e397a01adf01557a45e9027447
hayleyhartman <hayleyhartman@Hayleys-MacBook-Pro.local>
2019-02-01 13:05:45 -0500

    finished

---
+++ javascript.js
@@ -0,0 +1,79 @@
+$(function () {
+
+
+    const toggleClass = () => {
+        let prevActiveBtn = document.getElementsByClassName('b-active')
+        $(prevActiveBtn).removeClass('b-active')
+        let prevActiveP = document.getElementsByClassName('p-active')
+        $(prevActiveP).removeClass('p-active')
+    }
+
+    $(".buttonGroupItem").click(function () {
+        toggleClass()
+
+        if (this.id === 'btnOne') {
+            $(this).addClass('b-active')
+            $('#pOne').addClass('p-active')
+        } else if (this.id === 'btnTwo') {
+            $(this).addClass('b-active')
+            $('#pTwo').addClass('p-active')
+        } else {
+            $(this).addClass('b-active')
+            $('#pThree').addClass('p-active')
+        }
+
+    })
+
+    const locations = [{
+            name: "Cleveland",
+        },
+        {
+            name: "Houston",
+        },
+        {
+            name: "Dallas",
+        },
+        {
+            name: "New York City",
+        },
+        {
+            name: "Columbus",
+        },
+        {
+            name: "Chicago",
+        },
+        {
+            name: "Los Angeles",
+        },
+        {
+            name: "Denver",
+        }
+    ]
+
+    locations.map(({
+        name,
+        url
+    }) => {
+        const dataList = document.getElementById('places');
+
+        let option = document.createElement('option');
+
+        option.setAttribute('value', name);
+
+        option.setAttribute('href', url)
+
+        dataList.appendChild(option);
+
+        return `${name}: ${url}`
+    });
+
+    document.getElementById('searchBar').addEventListener('keyup', function(e) {
+        console.log(event.keycode)
+
+        if (event.keyCode === 13) {
+            const input = document.getElementById('searchBar').value
+            window.open(`https://www.google.com/maps/place/${input}`,'_blank');
+        }
+    })
+
+});
